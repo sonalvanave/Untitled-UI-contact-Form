91009c09ebca97029b83738f4ef7ded8704bac50
sonalvanave <vanvesonal@proton.me>
2024-12-22 09:29:37 +0000

    done

--- src/pages/Submission.jsx
+++ src/pages/Submission.jsx
@@ -1,8 +1,25 @@
+import Header from "@/components/Header";
 
-import React from "react";
+const Home = () => {
+  return (
+    <div className="w-full p-4">
+      <Header />
 
-function Submission() {
-  return <div>Submission Page</div>;
-}
+      <div className="mx-automax-w-7xl rounded-lg bg-lime-400 p-4">
+        <h1 className="text-3xl font-bold">Submission Completed 🎉</h1>
+        <p>
+          Form is submitted successfully! Go and visit here{" "}
+          <a
+            target="_blank"
+            className="text-blue-700"
+            href={import.meta.env.VITE_ENTRIES}
+          >
+            here.
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Submission;
+export default Home;
